1d31ccd397b55d169ae11cc06fb55943b7f62af8
noey <>
2018-03-10 17:00:56 +0700

    initial

---
+++ app/src/main/java/com/example/noey/findmyphone/Login.kt
@@ -0,0 +1,12 @@
+package com.example.noey.findmyphone
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by noey on 10/3/2018 AD.
+ */
+class Login: AppCompatActivity(){
+
+}
