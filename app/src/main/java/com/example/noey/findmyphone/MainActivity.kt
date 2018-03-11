6e8759f88c00a58d8347a0a9feaa354a89938e50
noey <n.noeyyy@gmail.com>
2018-03-11 11:41:28 +0700

    Added menu of main page

--- app/src/main/java/com/example/noey/findmyphone/MainActivity.kt
+++ app/src/main/java/com/example/noey/findmyphone/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.noey.findmyphone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,30 @@ class MainActivity : AppCompatActivity() {
 
         val usersData = UsersData(this)
         usersData.loadPhoneNumber()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return  super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item!!.itemId){
+            R.id.addTracker ->{
+                val intent = Intent(this, Tracker::class.java)
+                startActivity(intent)
+            }
+
+            R.id.help ->{
+
+            }
+
+            else ->{
+                return super.onOptionsItemSelected(item)
+            }
+        }
+
+        return true
     }
 }
